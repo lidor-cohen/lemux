48f50265c6c72a05c362a2a96f5749250ea59415
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-05-28 13:51:33 +0300

    change for pull request

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+import "./index.css";
+
+import App from "./components/App/App.jsx";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App/App.jsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
