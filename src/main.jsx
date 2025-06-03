c8fda51717bebeb298d77fd549bcbe1b0d9ea3fb
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-06-03 15:47:36 +0300

    add: local fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+import "./vendor/fonts/fonts.css";
+import "./index.css";
+
+import App from "./components/App/App.jsx";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./components/App/App.jsx";
 import { BrowserRouter } from "react-router";
 
 // stage-1-frontend-api
